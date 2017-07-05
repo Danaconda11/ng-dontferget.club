18cfb559f00abf1ce2c664cfcd4311758992e7c9
Josh Vanderwillik <joshwillik@gmail.com>
2017-07-05 17:10:18 -0400

    replace express-static with serve-static

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const serve_static = require('express-static')
+const serve_static = require('serve-static')
 let app = express()
 app.get('/hi', (req, res) => res.send('hi man'))
 app.use(serve_static(__dirname+'/public'))
