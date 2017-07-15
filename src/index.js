3218e27e2f3640fbcdc028e33cdd707bf9524010
Josh Vanderwillik <joshwillik@gmail.com>
2017-07-15 17:03:22 -0400

    remove testing code

--- src/index.js
+++ src/index.js
@@ -4,12 +4,8 @@ const init = require('./init')
 const serve_static = require('serve-static')
 const todos = require('./route_handlers/todos')
 let app = express()
-app.get('/hi', (req, res) => res.send('hi man'))
 app.get('/api/todos', todos.get_all)
 app.use(serve_static(__dirname+'/public'))
-// app.use(function (err, req, res, next) {
-//   console.error(res);
-// })
 app.listen(80, () => console.log('listening on :80'))
 if (config.debug) {
   init().catch(e => {
