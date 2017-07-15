42dc758a79431b08f1c500f515816bc9de7bc5fc
Josh Vanderwillik <joshwillik@gmail.com>
2017-07-15 17:27:48 -0400

    only allow access to the app if the user has been authenticated

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ const config = require('./config')
 const init = require('./init')
 const serve_static = require('serve-static')
 const todos = require('./route_handlers/todos')
+const auth = require('./middleware/auth')
 let app = express()
+app.use(serve_static(__dirname+'/public', {index: false}))
+app.get('/', auth.require_auth({otherwise: '/login'}),
+  auth.require_no_auth({otherwise: '/lists'}))
+app.use(auth.require_auth({otherwise: '/login'}))
 app.get('/api/todos', todos.get_all)
-app.use(serve_static(__dirname+'/public'))
 app.listen(config.http_port,
   () => console.log(`Listening on 0.0.0.0:${config.http_port}`))
 if (config.debug) {
---
+++ src/middleware/auth.js
@@ -0,0 +1,24 @@
+let E = module.exports
+E.require_auth = ({otherwise}) => {
+  return (req, res, next) => {
+    if (req.user) {
+      return next()
+    }
+    if (otherwise) {
+      return res.redirect(otherwise)
+    }
+    throw new Error('This code path is not implemented')
+  }
+}
+
+E.require_no_auth = ({otherwise}) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return next()
+    }
+    if (otherwise) {
+      return res.redirect(otherwise)
+    }
+    throw new Error('This code path is not implemented')
+  }
+}
