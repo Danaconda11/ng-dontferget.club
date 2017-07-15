703e820797f73015b4028e5ed8bdc83126fb5945
Josh Vanderwillik <joshwillik@gmail.com>
2017-07-15 17:05:58 -0400

    move hard coded port into config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   debug: process.env.DEBUG,
+  http_port: 80,
   mongo_host: process.env.MONGO_HOST,
   mongo_database: process.env.MONGO_DATABASE
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ const todos = require('./route_handlers/todos')
 let app = express()
 app.get('/api/todos', todos.get_all)
 app.use(serve_static(__dirname+'/public'))
-app.listen(80, () => console.log('listening on :80'))
+app.listen(config.http_port,
+  () => console.log(`Listening on 0.0.0.0:${config.http_port}`))
 if (config.debug) {
   init().catch(e => {
     console.error(e)
