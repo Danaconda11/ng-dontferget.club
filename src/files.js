828a439e65accdf312cb78cac43ec31e776efe52
Josh Vanderwillik <joshwillik@gmail.com>
2017-07-15 17:59:03 -0400

    replace custom promise with util.promisify

--- src/files.js
+++ src/files.js
@@ -1,15 +1,11 @@
 const serve_static = require('serve-static')
 const path = require('path')
+const util = require('util')
 let E = module.exports
 let base_dir = path.join(__dirname, 'public')
 
 E.static = () => serve_static(base_dir, {index: false})
 
-E.send = (res, file) => new Promise((resolve, reject) => {
-  res.sendFile(file, {root: base_dir}, e => {
-    if (e) {
-      return reject(e)
-    }
-    resolve()
-  })
-})
+E.send = (res, file) => {
+  return util.promisify(res.sendFile.bind(res))(file, {root: base_dir})
+}
