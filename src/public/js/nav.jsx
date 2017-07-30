5f08e06394714a32689c50b10afd5c21bfbc5a62
Josh Vanderwillik <joshwillik@gmail.com>
2017-07-30 14:58:39 -0400

    use better lists icon

--- src/public/js/nav.jsx
+++ src/public/js/nav.jsx
@@ -4,7 +4,7 @@ import {NavLink} from 'react-router-dom'
 export default () => (
   <div>
     <NavLink to="/lists" className="nav_link" title="Lists">
-      <i className="fa fa-2x fa-list"/></NavLink>
+      <i className="fa fa-2x fa-check-square"/></NavLink>
     <NavLink to="/games/chess" className="nav_link" title="Dan's chess stats">
       <i className="fa fa-2x fa-trophy"/></NavLink>
     <NavLink to="/account" className="nav_link" title="Account">
