406f2f98f1599d6f3f3cf788fe522991226e832d
Daniel <dbaldesarra9@gmail.com>
2017-08-13 21:45:23 -0400

    initalized form component with submit method

--- src/public/js/add_todo_component.js
+++ src/public/js/add_todo_component.js
@@ -1,20 +1,19 @@
 angular.module('lists')
 .component('todoForm', {
   templateUrl: '/views/todos_form.html',
-  controller: add_todo,
-  binding: {
-    add_todo: '&'
-  }
+  controller: add_todo_ctrl
 })
-function add_todo(api_service) {
-  let input_text = this.text
-  api_service({
-    url: 'api/todos',
-    method: 'POST',
-    data: {title: input_text}
-  }).then(res => {
-    console.log('inserted');
-  }).catch(e => {
-    console.log(e);
-  })
+function add_todo_ctrl(api_service) {
+  this.submit = function() {
+    let input_text = this.text
+    api_service({
+      url: 'api/todos',
+      method: 'POST',
+      data: {title: input_text}
+    }).then(res => {
+      console.log('inserted');
+    }).catch(e => {
+      console.log(e);
+    })
+  }
 }
